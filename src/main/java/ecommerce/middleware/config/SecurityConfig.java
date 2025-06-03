5b7c82fa230894d5a27ceefe8547b84f1a0e6675
Ana Clara Nobre <anaclarapnobre@gmail.com>
2025-06-03 11:20:58 -0300

    change config

--- src/main/java/ecommerce/middleware/config/SecurityConfig.java
+++ src/main/java/ecommerce/middleware/config/SecurityConfig.java
@@ -41,9 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-            "http://52.67.254.235:8888"  // origem do frontend
-        ));
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
