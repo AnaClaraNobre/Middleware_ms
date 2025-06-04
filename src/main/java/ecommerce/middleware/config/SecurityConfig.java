8248a7ce9e4f64b3321f53857c240f863c9d3933
Angelo Barcelos <angelorodrigues318@gmail.com>
2025-06-04 02:51:32 -0300

    inferno

--- src/main/java/ecommerce/middleware/config/SecurityConfig.java
+++ src/main/java/ecommerce/middleware/config/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/product-images/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/product/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/product-image/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
