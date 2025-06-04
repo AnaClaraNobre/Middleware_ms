1be2aba9b5fe44a3817ca78b947787ba48cf5940
Angelo Barcelos <angelorodrigues318@gmail.com>
2025-06-04 02:35:36 -0300

    teste triste

--- src/main/java/ecommerce/middleware/config/SecurityConfig.java
+++ src/main/java/ecommerce/middleware/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
 
 import ecommerce.middleware.jwt.JwtAuthenticationFilter;
 
@@ -31,6 +32,8 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/product/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/product-image/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
